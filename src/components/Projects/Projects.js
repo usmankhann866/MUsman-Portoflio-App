30d3bb86cc61f8aaf9eacba2e424216206d04896
Mohammad Usman <usmankhan866@gmail.com>
2022-11-01 13:49:20 +0500

    changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
+// import leaf from "../../Assets/Projects/leaf.png";
+// import emotion from "../../Assets/Projects/emotion.png";
 import faceReg from "../../Assets/Projects/face4.jfif";
 import htmlCode from "../../Assets/Projects/imageTohtml.jpg";
-import suicide from "../../Assets/Projects/suicide.png";
+// import suicide from "../../Assets/Projects/suicide.png";
 import restaurant from "../../Assets/Projects/Resturant.png";
 
 function Projects() {
